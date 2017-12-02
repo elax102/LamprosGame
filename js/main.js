b089b446585bc8bcded6b824caf5340a0ac2aef2
Alex Davis <davis.alex.jr@gmail.com>
2017-12-02 14:29:38 -0500

    made game canvas larger

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+var game = new Phaser.Game(1500, 800, Phaser.AUTO, '');
 game.state.add('Menu', MenuState);
 game.state.add('Game', GameState);
 game.state.start('Menu');
