0458cc575355955446d79bd6f4ee3ee11b1ff0b7
lufinkey <luisfinke@gmail.com>
2017-12-02 14:17:00 -0500

    fix reference to nonexistant state

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 game.state.add('Menu', MenuState);
 game.state.add('Game', GameState);
 game.state.start('Menu');
